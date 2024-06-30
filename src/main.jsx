fbb549db9ff359d680b8670b27291fe009f3a559
jonomnom <jonomnom5097@gmail.com>
2024-06-29 23:01:43 -0700

    hopefully fix to only dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "@coinbase/onchainkit/styles.css";
+
+localStorage.theme = 'light'
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
