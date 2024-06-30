3f630893e11b1a82e886f6b10b39c1c0cffcb6a3
jonomnom <jonomnom5097@gmail.com>
2024-06-30 11:15:24 -0700

    save

--- src/routes/public.jsx
+++ src/routes/public.jsx
@@ -3,11 +3,12 @@ import { lazy } from "react";
 // project import
 import Loadable from "../components/Loadable";
 import { PublicLayout } from "../components/layouts/publicLayout";
-
+import MarketPlace from "@/pages/MarketPlace";
+import Library from "@/pages/Library";
 // render - login
 const Home = Loadable(lazy(() => import("../pages/Home")));
-const Library = Loadable(lazy(() => import("../pages/Library")));
-const MarketPlace = Loadable(lazy(() => import("../pages/MarketPlace")));
+// const Library = Loadable(lazy(() => import("../pages/Library")));
+// const MarketPlace = Loadable(lazy(() => import("../pages/MarketPlace")));
 
 const withLayout = (component) => <PublicLayout>{component}</PublicLayout>;
 
